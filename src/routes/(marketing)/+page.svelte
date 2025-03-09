734d12b2d0835b93604f709bdda7060f58c80b35
Rinat Enikeev <rinat.enikeev@gmail.com>
2025-03-09 19:38:26 +0200

    remove dobro highlight

--- src/routes/(marketing)/+page.svelte
+++ src/routes/(marketing)/+page.svelte
@@ -27,12 +27,6 @@
   <div class="hero-content text-center py-12">
     <div class="max-w-xl">
       <div
-        class="text-xl md:text-3xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-primary to-accent mb-3 md:mb-7 pb-1"
-      >
-        DoBro City
-      </div>
-
-      <div
         class="text-4xl md:text-6xl font-bold px-2"
         style="line-height: 1.2;"
       >
